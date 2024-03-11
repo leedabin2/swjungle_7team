2f48d8d9add5795cb83b8f105bef728dba948038
최준성 <147041315+yumXD@users.noreply.github.com>
2024-03-11 21:27:44 +0900

    feat: 간단한 로그인 페이지 구현

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from settings import ca_path
 from pymongo import MongoClient  
 import certifi
@@ -11,6 +11,10 @@ db = client.dbsparta
 @app.route('/')
 def home():
    return 'This is Home!'
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)

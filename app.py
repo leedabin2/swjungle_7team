0d377ecb2296e0dedba8d9b07fbe4e6b1aa1cc4f
최준성 <147041315+yumXD@users.noreply.github.com>
2024-03-12 21:19:37 +0900

    test: 토큰 엔드포인트 테스트

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ from pymongo import MongoClient
 import xml.etree.ElementTree as elemTree
 import certifi,hashlib
 import requests
-from flask_cors import CORS
 
 app = Flask(__name__)
 jwt = JWTManager(app)
-CORS(app)
 
 tree = elemTree.parse('keys.xml')
 secretkey = tree.find('string[@name="secret_key"]').text
@@ -49,8 +47,8 @@ def register():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-      username_receive = request.form['username_input']
-      password_receive = request.form['password_input']
+      username_receive = request.json.get('username_input')
+      password_receive = request.json.get('password_input')
    
       password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
       
@@ -63,7 +61,7 @@ def login():
       expires_delta = datetime.timedelta(minutes=30)
       access_token = create_access_token(identity=username_receive, expires_delta=expires_delta)
       # 클라이언트에 200과 함께 토큰 전송
-      return jsonify({'result': 'success', 'token': access_token}), 200
+      return jsonify({'result': 'success', 'access_token': access_token}), 200
     else:
         # GET 요청을 처리하기 위한 로직 추가
       return render_template('login.html') 
@@ -92,12 +90,12 @@ def search_local():
     else:
         return jsonify({'error': '클라이언트 아이디와 클라이언트 시크릿이 요청 헤더에 포함되어야 합니다.'}), 400
 
-# 토큰의 유효성 검사
-@app.route('/a', methods = ['GET'])
-@jwt_required()
-def my_page():
-    current_user = get_jwt_identity()
-    return jsonify(msg=f'Welcome, {current_user}')
+# 보호된 엔드포인트
+@app.route('/protected', methods=['GET'])
+@jwt_required()  # JWT 필요
+def protected():
+    current_user = get_jwt_identity()  # 현재 사용자 식별자
+    return jsonify(logged_in_as=current_user), 200
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)

d0571174062030d0a2e11a5ac0c5bb31fefb6969
최준성 <147041315+yumXD@users.noreply.github.com>
2024-03-14 00:15:35 +0900

    feat: 맛집 등록 로그인 필요 검증 기능 구현

--- app.py
+++ app.py
@@ -177,5 +177,12 @@ def post_up_button_count():
   db.registerlist.update_one({"key": int(key_receive)}, {"$inc": {"number": 1}})
   return jsonify(), 200
 
+# 보호된 엔드포인트
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user)
+
 if __name__ == '__main__':
   app.run('0.0.0.0',port=5000,debug=True)
